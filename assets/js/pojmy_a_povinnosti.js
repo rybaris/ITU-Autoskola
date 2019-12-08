b5aec927af32f54066061a105deded581cf7981a
rybaris <info@ondrejsebesta.com>
2019-12-08 20:33:53 +0100

    Dalsi upravy

---
+++ assets/js/pojmy_a_povinnosti.js
@@ -0,0 +1,77 @@
+var current = 0;
+
+$("#prevTheory").click(prevRender);
+$("#nextTheory").click(nextRender);
+
+var queSrc = [
+    "<p>'Krajnice':</p>",
+    "<p>'Zastavit vozidlo' znamená:</p>",
+    "<p>'Dálnice'</p>",
+    "<p>Nesmět omezit znamená povinnost:</p>",
+    "<p>Po pozemní komunikaci jde osoba, která vede psa. Vztahují se na ni povinnosti průvodce vedených zvířat?</p>"
+
+]
+
+var ansSrc = [
+    "je část povrchu pozemní komunikace ležící mezi okrajem přilehlého jízdního pruhu a hranou koruny pozemní komunikace, skládá se zpravidla ze zpevněné a nezpevněné části.",
+    "přerušit jízdu z důvodu nezávislého na vůli řidiče.",
+    "je pozemní komunikace označená dopravní značkou 'Dálnice'",
+    "počínat si tak, aby jinému účastníku provozu na pozemních komunikacích nebylo nijak překáženo.",
+    "NE"
+
+]
+
+function render() {
+
+    if (queSrc[current].startsWith('assets')) {
+
+        var img = $("<img>").attr({
+            id: "theoryImg",
+            src: queSrc[current],
+        });
+
+    } else {
+
+        var img = $(".question").innerHTML = queSrc[current];
+
+    }
+
+    var text = $("#rightAnswer").innerHTML = ansSrc[current];
+
+    $('.question').html(img);
+    $('#rightAnswer').html(text);
+
+    console.log(queSrc.length);
+    let progress = 100 / (queSrc.length-1);
+    $('#progress-bar').width((current * progress) + "%");
+
+}
+    
+function prevRender() {
+
+    event.preventDefault()
+
+    if (current === 0) {
+        current = queSrc.length-1;
+        current++;
+    }
+
+    current = current - 1;
+
+    render();
+
+}
+
+function nextRender() {
+
+    event.preventDefault()
+
+    if (current === (queSrc.length - 1)) {
+        current = 0;
+        current--;
+    }
+
+    current = current + 1;
+    render();
+
+}
