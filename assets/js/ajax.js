f30baba4bc16f2860c6ac42c7c43302051347267
rybaris <info@ondrejsebesta.com>
2020-01-15 00:10:50 +0100

    dalsi upravy

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,50 +1,88 @@
+let current = 0;
+let question = document.getElementById('question');
+let answers = document.getElementById('answers');
+
+$("#prevTheory").click(prevRender);
+$("#nextTheory").click(nextRender);
+
+// TODO DATA 
+// BODY
+// SPRAVNA ODPOVED
+
 function ajaxFunction() {
 
-    let current = 2;
-    let question = document.getElementById('question');
-    let answers = document.getElementById('answers');
     let req = new XMLHttpRequest();
-    let p;
-
-    // Funkce porovna datovy typ a dle nej sestavi HTML otazky
-    function getQuestionByType(index) {
-
-        if(p[index].type == "img") {
-            question.innerHTML = '<img id="image-data"></img>';
-            document.getElementById('image-data').src = p[index].question;
-        } else {
-            question.innerHTML = '<p id="text-data"></p>';
-            document.getElementById('text-data').innerHTML = p[index].question;
-        }
-
-    }
-
-    function getChoice(index) {
-
-        let paragraph, j;
-        
-        paragraph = document.createElement('p');
-        paragraph.className = "answer-text";
-        answers.appendChild(paragraph);
-        document.getElementsByClassName('answer-text').innerHTML = p[index].answer1;
-        
-        //document.getElementsByClassName('answer-text').innerHTML = p[index].answer2
-        //document.getElementsByClassName('answer-text').innerHTML = p[index].answer3
-    }
-
     req.open("GET", "https://raw.githubusercontent.com/rybaris/ITU-Autoskola/master/assets/js/data.json", true)
-
     req.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-
     req.onreadystatechange = () => {
         if (req.readyState == 4 && req.status == 200) {
-            p = JSON.parse(req.responseText);
-            getQuestionByType(current);
-            getChoice(current);
+            let p = JSON.parse(req.responseText);
+            getQuestionByType(p, current);
+            getChoices(p, current);
         }
     }
-
     req.send()
     
+}
+
+// Funkce porovna datovy typ a dle nej sestavi HTML otazky
+function getQuestionByType(item, index) {
+
+    if(item[index].type == "img") {
+        question.innerHTML = '<img id="image-data"></img>';
+        document.getElementById('image-data').src = item[index].question;
+    } else {
+        question.innerHTML = '<p id="text-data"></p>';
+        document.getElementById('text-data').innerHTML = item[index].question;
+    }
+
+}
+
+// Funkce vlozi mozne odpovedi na otazku
+function getChoices(item, index) {
+    
+    document.getElementById('answer1').innerHTML = item[index].answer1;
+    document.getElementById('answer2').innerHTML = item[index].answer2;
+    document.getElementById('answer3').innerHTML = item[index].answer3;
+
+}
+
+function showTruth() {
+    for (let i = 0;i <= 3 ;i++) {
+        
+    }
+}
+
+// Funkce tlacitka "Predchozi"
+function prevRender() {
+
+    event.preventDefault()
+
+    if (current === 0) {
+        document.getElementById('prevTheory').style.display = "none";
+        console.log(current);
+    } else {
+        current = current - 1;
+        console.log(current);
+    }
+
+    ajaxFunction();
+    
+
+}
+
+// Funkce tlacitka "Dalsi"
+function nextRender() {
+
+    event.preventDefault()
+    console.log(current);
+
+    //if (current === (queSrc.length - 1)) {
+    //   current = 0;
+    //   current--;
+    //}
+
+    current = current + 1;
+    ajaxFunction();
 
 }
